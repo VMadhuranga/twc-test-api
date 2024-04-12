bba75cba381f08301855b293fc558dd4e304add1
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-12 20:31:24 +0530

    Add userRouter to app's middleware chian

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middlewares/error-handler");
 const connectDb = require("./config/dbConfig");
+const userRouter = require("./routes/user-route");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/users", userRouter);
 
 app.use(errorHandler());
 
