61e98e141101ef530894d7eb094de183cbe22bc0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-12 20:31:24 +0530

    Add userRouter to app's middleware chain
    Additionally fixed errorHandler invocation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middlewares/error-handler");
 const connectDb = require("./config/dbConfig");
+const userRouter = require("./routes/user-route");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(errorHandler());
+app.use("/users", userRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
