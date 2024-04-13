a44b2824907fe51166a9c92c692a4d3d9c49ef29
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 18:44:35 +0530

    Create router to handle GET request in /logout path

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/login", authController.login);
 router.get("/refresh", authController.refresh);
+router.get("/logout", authController.logout);
 
 module.exports = router;
