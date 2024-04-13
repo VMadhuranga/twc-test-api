f7bae241d1196b417b0e1f621dd732c5263561f6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 18:17:03 +0530

    Create router to handle GET request in /refresh path

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth-controller/");
 const router = express.Router();
 
 router.post("/login", authController.login);
+router.get("/refresh", authController.refresh);
 
 module.exports = router;
