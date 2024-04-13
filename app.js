3dd1abab32149264211b29f19610ae7f0ce3764f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 15:48:43 +0530

    Add authRouter to app's middleware chian

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const errorHandler = require("./middlewares/error-handler");
 const connectDb = require("./config/dbConfig");
 const userRouter = require("./routes/user-route");
+const authRouter = require("./routes/auth-route");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/", authRouter);
 app.use("/users", userRouter);
 
 app.use(errorHandler);
