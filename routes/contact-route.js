6bea9df877e21549e72db5f251327ad53d539dc6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 13:21:34 +0530

    Create router to handle DELETE request in /contacts/:contact_id path

--- routes/contact-route.js
+++ routes/contact-route.js
@@ -6,5 +6,9 @@ const router = express.Router();
 router.post("/:user_id/contacts", contactController.createContact);
 router.get("/:user_id/contacts", contactController.getContacts);
 router.put("/:user_id/contacts/:contact_id", contactController.updateContact);
+router.delete(
+  "/:user_id/contacts/:contact_id",
+  contactController.deleteContact,
+);
 
 module.exports = router;
