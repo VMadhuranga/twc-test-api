815b277ed422c7269e4dbb6c150d356a2439367e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 12:23:13 +0530

    Add updateContact function to contactController index

--- controllers/contact-controller/index.js
+++ controllers/contact-controller/index.js
@@ -1,7 +1,9 @@
 const createContact = require("./create-contact");
 const getContacts = require("./get-contacts");
+const updateContact = require("./update-contact");
 
 module.exports = {
   createContact,
   getContacts,
+  updateContact,
 };
