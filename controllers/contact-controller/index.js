265728b39756aa48dc40b87bf88dbc8b13dfba15
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 13:21:10 +0530

    Add deleteContact function to contactController index

--- controllers/contact-controller/index.js
+++ controllers/contact-controller/index.js
@@ -1,9 +1,11 @@
 const createContact = require("./create-contact");
 const getContacts = require("./get-contacts");
 const updateContact = require("./update-contact");
+const deleteContact = require("./delete-contact");
 
 module.exports = {
   createContact,
   getContacts,
   updateContact,
+  deleteContact,
 };
