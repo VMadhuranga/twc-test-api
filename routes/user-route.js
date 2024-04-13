42cfa673100aefc1dd496636513f057fad65f1ae
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 18:18:31 +0530

    Add authenticate middleware to userRouter's middleware chain

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/user-controller/");
 const contactRouter = require("./contact-route");
+const authenticate = require("../middlewares/authenticate");
 
 const router = express.Router();
 
 router.post("/", userController.createUser);
 
+router.use(authenticate);
 router.use("/", contactRouter);
 
 module.exports = router;
