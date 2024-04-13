22b8c545ed2748451dcebf46548816e15c74a51e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 18:43:42 +0530

    Create logout function

---
+++ controllers/auth-controller/logout.js
@@ -0,0 +1,19 @@
+// GET /logout
+const logout = (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) {
+    return res.sendStatus(204);
+  }
+
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ message: "Cookie cleared" });
+};
+
+module.exports = logout;
