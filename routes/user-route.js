354298e0de1a51beccff7e6a309110e36d55590b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-13 09:56:25 +0530

    Add contactRouter to userRouter's middleware chain

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/user-controller/");
+const contactRouter = require("./contact-route");
 
 const router = express.Router();
 
 router.post("/", userController.createUser);
+
+router.use("/", contactRouter);
 
 module.exports = router;
