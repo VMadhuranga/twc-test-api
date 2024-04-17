8fdd896cf9ec72e7dfb5b9ab09d22c28e9d66a57
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 15:01:12 +0530

    Create router to handle GET request in contacts/:contact_id path

--- routes/contact-route.js
+++ routes/contact-route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/:user_id/contacts", contactController.createContact);
 router.get("/:user_id/contacts", contactController.getContacts);
+router.get("/:user_id/contacts/:contact_id", contactController.getContact);
 router.put("/:user_id/contacts/:contact_id", contactController.updateContact);
 router.delete(
   "/:user_id/contacts/:contact_id",
