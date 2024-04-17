9b8c914b1b0bacebd084c8fa220b96a24a7e7236
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 14:55:43 +0530

    Update condition to check for cookie instead authorization header

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,20 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
+  const token = req.cookies?.jwt;
 
-  if (!authHeader?.startsWith("Bearer")) {
+  if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const accessToken = authHeader.split(" ")[1];
-
-  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: "Forbidden" });
     }
 
-    req.userId = decoded.userId;
     next();
   });
 };
