2496d678122855f07ef429718481f3138a7a1e0e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 14:59:05 +0530

    Update getContacts function validations

--- controllers/contact-controller/get-contacts.js
+++ controllers/contact-controller/get-contacts.js
@@ -5,19 +5,17 @@ const ContactModel = require("../../models/contact-model");
 
 // GET /users/:user_id/contacts
 const getContacts = [
-  param("user_id").trim().escape(),
+  param("user_id").trim().isMongoId().escape(),
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .json({ message: "Validation error", data: errors.array() });
+      return res.status(404).json({ message: "Resource not found" });
     }
 
     const [user, contacts] = await Promise.all([
-      UserModel.findById(req.params.user_id).lean().exec(),
+      UserModel.findById(req.params.user_id).exec(),
       ContactModel.find({
         portalOwnerId: req.params.user_id,
       }).exec(),
